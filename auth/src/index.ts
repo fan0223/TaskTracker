13da81eb9a457bb2b029a54b87a0c9878ce6af56
fan0223 <louie8653515@yahoo.com.tw>
2022-06-26 15:17:31 +0800

    init commit

---
+++ auth/src/index.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+import { app } from './app'
+const start = async () => {
+  console.log('Starting up.......')
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined')
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI must be defined')
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 50000
+    })
+
+    // when mongoose start , delete all collections , prevent mongoDB bug
+    const collections = await mongoose.connection.db.collections()
+
+    for (let collection of collections) {
+      await collection.deleteMany({})
+    }
+
+
+    console.log('Connected to mongoDB')
+  } catch (error) {
+    console.error(error)
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000.')
+  })
+}
+
+start()
