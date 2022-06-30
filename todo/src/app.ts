6b5e6f17b447dfda787a9761ff08511f89509bf8
fan0223 <louie8653515@yahoo.com.tw>
2022-07-01 00:45:05 +0800

    todo new route

--- todo/src/app.ts
+++ todo/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express'
 import 'express-async-errors'
 import mongoose from 'mongoose'
-import { NotFoundError, errorHandler } from '@fan-todo/common'
+import cookieSession from 'cookie-session'
+
+import { NotFoundError, errorHandler, currentUser } from '@fan-todo/common'
 import { indexTodoRouter } from './route/index'
 import { newTodoRouter } from './route/new'
 import { showTodoRouter } from './route/show'
@@ -10,7 +12,13 @@ import { updateTodoRouter } from './route/update'
 const app = express()
 app.set('trust proxy', true)
 app.use(express.json())
+app.use(cookieSession({
+  signed: false,
+  // secure: false
+  secure: process.env.NODE_ENV !== 'test'
+}))
 
+app.use(currentUser)
 
 app.use(indexTodoRouter)
 app.use(newTodoRouter)
--- todo/src/route/new.ts
+++ todo/src/route/new.ts
@@ -1,11 +1,35 @@
 import express, { Response, Request } from 'express'
+import { Todo } from '../model/todo'
+import { requireAuth, validateRequest } from '@fan-todo/common'
+import { body } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/api/todo', async (req: Request, res: Response) => {
+router.post('/api/todo',
+  requireAuth,
+  [
+    body('title')
+      .not()
+      .isEmpty()
+      .withMessage('Title is required'),
+    body('content')
+      .not()
+      .isEmpty()
+      .withMessage('Title is required')
+  ],
+  validateRequest
+  , async (req: Request, res: Response) => {
+    const { title, content } = req.body
 
+    const todo = Todo.build({
+      title,
+      content,
+      userId: req.currentUser!.id
+    })
 
-  res.send('new')
-})
+    await todo.save()
+
+    res.status(201).send(todo)
+  })
 
 export { router as newTodoRouter }
