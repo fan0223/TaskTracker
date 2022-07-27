b5be963d4b947e767fa704969600be6bf0c6c250
fan0223 <louie8653515@yahoo.com.tw>
2022-07-28 07:13:05 +0800

    Updates

--- common/src/events/base-consumer.ts
+++ common/src/events/base-consumer.ts
@@ -1,6 +1,7 @@
 import { Consumer, Message } from 'redis-smq'
 import { Subjects } from './subjects';
 import { ICallback } from 'redis-smq-common/dist/types';
+import { TodoCreatedEvent } from './todo-created-event'
 
 interface Event {
   subject: Subjects,
@@ -16,26 +17,35 @@ export abstract class CustomConsumer<T extends Event> {
   }
 
 
-  // listen() {
-  //   this.consumer.consume(
-  //     this.queueName,
-  //     this.messageHandler,
-  //     (err) => {
-  //       if (err) console.log(err)
-  //     }
-  //   )
-  //   this.consumer.run()
-  // }
+  listen() {
+    // this.consumer.consume(
+    //   this.queueName,
+    //   this.messageHandler,
+    //   (err) => {
+    //     if (err) console.log(err)
+    //   }
+    // )
+    this.consumer.run()
+  }
 }
 
-// class CommentCreatedConsumer extends CustomComsumer<CommentCreatedEvent>{
-//   queueName: Subjects.CommentCreated = Subjects.CommentCreated;
+// export class TodoCreatedConsumer extends CustomConsumer<TodoCreatedEvent>{
+//   queueName: Subjects.TodoCreated = Subjects.TodoCreated;
 //   messageHandler: (msg: Message, cb: ICallback<void>) => void = async (msg, cb) => {
 //     const payload = msg.getBody()
 //     console.log('Message payload ', payload)
 //     cb()
 //   }
-// }
+//   constructor() {
+//     super()
+//     this.consumer.consume(
+//       this.queueName,
+//       this.messageHandler,
+//       (err) => {
+//         if (err) console.log(err)
+//       }
+//     )
+//   }
 
-// const a = new CommentCreatedConsumer()
-// a.listen()
+// }
+// new TodoCreatedConsumer().listen()
