f349e6972091b5379efc89cd3ab3fba1cccfc21e
fan0223 <louie8653515@yahoo.com.tw>
2022-07-31 05:33:06 +0800

    Updates

--- common/src/events/subjects.ts
+++ common/src/events/subjects.ts
@@ -1,7 +1,9 @@
 export enum Subjects {
-  TodoCreated = 'todoCreated',
-  TodoUpdated = 'todoUpdated',
-  TodoDeleted = 'todoDeleted',
-  CommentCreated = 'commentCreated',
-  CommentDeleted = 'commentDeleted',
+  Comment_TodoCreated = 'comment-todoCreated',
+  Comment_TodoDeleted = 'comment-todoDeleted',
+  Query_TodoCreated = 'query-todoCreated',
+  Query_TodoUpdated = 'query-todoUpdated',
+  Query_TodoDeleted = 'query-todoDeleted',
+  Query_CommentCreated = 'query-commentCreated',
+  Query_CommentDeleted = 'query-commentDeleted',
 }
