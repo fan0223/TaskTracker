c0773f947954dde08501baf3029af3f818fdd9f7
fan0223 <louie8653515@yahoo.com.tw>
2022-08-06 05:09:30 +0800

    Updates

--- common/src/events/query- comment-created-event.ts
+++ common/src/events/query- comment-created-event.ts
@@ -3,6 +3,7 @@ export interface Query_CommentCreatedEvent {
   subject: Subjects.Query_CommentCreated,
   data: {
     id: string,
+    todoId: string,
     userId: string,
     createdAt: string,
     content: string
