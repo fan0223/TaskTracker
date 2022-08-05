c37931a636b66d14996052678775af469aaf5515
fan0223 <louie8653515@yahoo.com.tw>
2022-08-06 05:14:00 +0800

    Updates

--- common/src/events/query-comment-delete-event.ts
+++ common/src/events/query-comment-delete-event.ts
@@ -3,6 +3,7 @@ export interface Query_CommentDeletedEvent {
   subject: Subjects.Query_CommentDeleted,
   data: {
     id: string,
+    todoId: string,
     userId: string,
     createdAt: string,
     content: string
