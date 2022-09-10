28ef34a25d3194a9803dd86905ad5ae4d9714808
fan0223 <louie8653515@yahoo.com.tw>
2022-09-11 06:08:18 +0800

    update aws

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,7 +6,7 @@ export default ({ req }) => {
     // http://ingress-nginx-controller.ingress-ngix.svc.cluster.local/api/users/currentuser
     return axios.create({
       baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+        'http://todo-app-load-balancer-56769992.ap-northeast-1.elb.amazonaws.com',
       // baseURL: 'http://www.ticketing-app-fan.xyz/', use domain url when deployment online
       headers: req.headers,
     });
