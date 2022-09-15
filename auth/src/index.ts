df5e30335a8ab98d6e6c11444c332e038303fd32
fan0223 <louie8653515@yahoo.com.tw>
2022-09-15 14:15:34 +0800

    update aws

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -10,6 +10,7 @@ const start = async () => {
 
   try {
     await mongoose.connect(process.env.MONGO_URI, {
+      tlsCAFile: `${__dirname}/certs/rds-combined-ca-bundle.pem`,
       serverSelectionTimeoutMS: 50000
     })
 
