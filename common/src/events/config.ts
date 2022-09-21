37c243bca658bd4cfa95b81b58816358f6c54282
fan0223 <louie8653515@yahoo.com.tw>
2022-09-21 15:53:22 +0800

    Updates

--- common/src/events/config.ts
+++ common/src/events/config.ts
@@ -29,7 +29,7 @@ export const Config: IConfig = {
         // : 'clustercfg.redis-server.kdo2wk.memorydb.ap-northeast-1.amazonaws.com',
         : 'todo-app-redis-001.kdo2wk.0001.apne1.cache.amazonaws.com:6379',
       // port: 6379
-      family: 6
+
     },
   },
   logger: {
