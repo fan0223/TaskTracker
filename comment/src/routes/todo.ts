1399678c02b9c395fe34c3a1158c6f1505e83165
fan0223 <louie8653515@yahoo.com.tw>
2022-09-26 02:01:31 +0800

    update aws

--- comment/src/routes/todo.ts
+++ comment/src/routes/todo.ts
@@ -4,9 +4,8 @@ import { Todo } from '../models/todo'
 const router = express()
 
 router.get('/api/todo/testurl/comment/testurl',
-  async (req: Request, res: Response) => {
-    const todo = await Todo.find({})
-    res.send(todo)
+  (req: Request, res: Response) => {
+    res.status(200)
   })
 
 export { router as todoRouter }
