e1ad0dd8b46569710ad4da6708092034537ae8d3
fan0223 <louie8653515@yahoo.com.tw>
2022-09-26 15:54:23 +0800

    update aws

--- comment/src/routes/todo.ts
+++ comment/src/routes/todo.ts
@@ -5,7 +5,7 @@ const router = express()
 
 router.get('/api/todo/testurl/comment/testurl',
   (req: Request, res: Response) => {
-    res.status(200)
+    res.status(200).send({})
   })
 
 export { router as todoRouter }
